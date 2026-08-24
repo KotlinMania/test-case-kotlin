dbf4a90817225b12dc2ddf2d32cceb161cedcfe8
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-23 23:46:32 -0700

    chore(swift): modernize Swift test harness to Swift Testing and Swift 6 toolchain

--- swift-test-harness/Tests/SwiftTestHarnessTests/TestCaseExportTests.swift
+++ swift-test-harness/Tests/SwiftTestHarnessTests/TestCaseExportTests.swift
@@ -1,25 +1,12 @@
-#if canImport(Testing)
 import Testing
 import TestCase
 
-@Suite("TestCase Swift Export Smoke Tests")
+@Suite("TestCase Swift Export Suite")
 struct TestCaseExportTests {
     @Test("Swift module loads and basic testCase runs")
     func swiftModuleLoads() {
-        let tc = TestCase_kt.testCase(arguments: "hello", name: "test1", ignored: false, expectedPanic: nil)
+        let tc = TestCase(arguments: nil, name: "test1", ignored: false, expectedPanic: nil)
         #expect(tc.name == "test1")
         #expect(tc.shouldRun)
     }
 }
-#elseif canImport(XCTest)
-import XCTest
-import TestCase
-
-final class TestCaseExportTests: XCTestCase {
-    func testSwiftModuleLoads() throws {
-        let tc = TestCase_kt.testCase(arguments: "hello", name: "test1", ignored: false, expectedPanic: nil)
-        XCTAssertEqual(tc.name, "test1")
-        XCTAssertTrue(tc.shouldRun)
-    }
-}
-#endif
